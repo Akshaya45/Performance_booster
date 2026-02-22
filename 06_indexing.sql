b4f254ca0714be626b3706c1793a97b86f9aa072
Akshaya Reddy <155704584+Akshaya45@users.noreply.github.com>
2026-02-22 15:26:42 +0530

    Update 06_indexing.sql

--- 06_indexing.sql
+++ 06_indexing.sql
@@ -1,13 +1,22 @@
 
---Index 1 – For Reports by Batch
+--Index 1 â€“ For Reports by Batch
 CREATE NONCLUSTERED INDEX idx_students_batch
 ON Students(batch);
 
 -- This index helps queries filtering by batch
 -- Example: AVG marks per batch
 
---Index 2 – For Marks-Based Queries
+--Index 2 â€“ For Marks-Based Queries
 -- This index helps sp_GetTopStudentsByBatch
 -- because it filters and sorts by marks
 CREATE NONCLUSTERED INDEX idx_students_marks
+
 ON Students(marks);
+
+
+-- SQL Server will automatically use this index to improve performance.
+SELECT *
+FROM Students
+WHERE batch = 2023
+ORDER BY marks DESC;
+
